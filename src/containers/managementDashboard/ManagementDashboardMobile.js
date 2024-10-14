fcc7c858aaa5c46875422f4748984e2e42eb65a4
LongLH <ro1462000@gmail.com>
2024-10-14 19:23:57 +0700

    LongLH

--- src/containers/managementDashboard/ManagementDashboardMobile.js
+++ src/containers/managementDashboard/ManagementDashboardMobile.js
@@ -51,101 +51,123 @@ const ManagementDashboardMobile = ({
           />
         </div>
       </Col>
-      <Col span={24} className="management-dashboard-mobile-col">
-        <div style={{ paddingTop: 30, height: 300 }}>
-          <ChartQuatity
-            isValueDashBoard={isValueDashBoard}
-            chooseLanguage={chooseLanguage}
-          />
-        </div>
-        <div style={{ display: "flex" }}>
-          <div style={{ width: "50%", display: "flex", alignItems: "center" }}>
-            <Tag color="#626262" style={{ width: 40, height: 20 }}></Tag>
+      <Col
+        span={24}
+        className="management-dashboard-mobile-col"
+        style={{ marginTop: 20 }}
+      >
+        <div style={{ padding: 10 }}>
+          <div>
             <span
-              className="management-dashboard-span-status"
-              data-status-color-processing={true}
-              data-mobile={true}
+              style={{
+                display: "flex",
+                fontWeight: 600,
+              }}
             >
-              {fileLanguage[chooseLanguage].processing}
+              {fileLanguage[chooseLanguage].quantity_of_checksheets}
             </span>
           </div>
-          <div
-            style={{
-              width: "50%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <span
-              className="management-dashboard-span-total"
-              data-total-color-processing={true}
-              data-mobile={true}
-            >
-              {isValueDashBoard.stt_processing === "[]"
-                ? 0
-                : isValueDashBoard.stt_processing}
-            </span>
+          <div style={{ paddingTop: 30, height: 300 }}>
+            <ChartQuatity
+              isValueDashBoard={isValueDashBoard}
+              chooseLanguage={chooseLanguage}
+            />
           </div>
-        </div>
-        <div style={{ display: "flex", paddingTop: 10 }}>
-          <div style={{ width: "50%", display: "flex", alignItems: "center" }}>
-            <Tag color="#085FA0" style={{ width: 40, height: 20 }}></Tag>
-            <span
-              className="management-dashboard-span-status"
-              data-status-color-completed={true}
-              data-mobile={true}
+          <div style={{ display: "flex" }}>
+            <div
+              style={{ width: "50%", display: "flex", alignItems: "center" }}
             >
-              {fileLanguage[chooseLanguage].completed}
-            </span>
+              <Tag color="#626262" style={{ width: 40, height: 20 }}></Tag>
+              <span
+                className="management-dashboard-span-status"
+                data-status-color-processing={true}
+                data-mobile={true}
+              >
+                {fileLanguage[chooseLanguage].processing}
+              </span>
+            </div>
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <span
+                className="management-dashboard-span-total"
+                data-total-color-processing={true}
+                data-mobile={true}
+              >
+                {isValueDashBoard.stt_processing === "[]"
+                  ? 0
+                  : isValueDashBoard.stt_processing}
+              </span>
+            </div>
           </div>
-          <div
-            style={{
-              width: "50%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <span
-              className="management-dashboard-span-total"
-              data-total-color-completed={true}
-              data-mobile={true}
+          <div style={{ display: "flex", paddingTop: 10 }}>
+            <div
+              style={{ width: "50%", display: "flex", alignItems: "center" }}
             >
-              {isValueDashBoard.stt_complete === "[]"
-                ? 0
-                : isValueDashBoard.stt_complete}
-            </span>
+              <Tag color="#085FA0" style={{ width: 40, height: 20 }}></Tag>
+              <span
+                className="management-dashboard-span-status"
+                data-status-color-completed={true}
+                data-mobile={true}
+              >
+                {fileLanguage[chooseLanguage].completed}
+              </span>
+            </div>
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <span
+                className="management-dashboard-span-total"
+                data-total-color-completed={true}
+                data-mobile={true}
+              >
+                {isValueDashBoard.stt_complete === "[]"
+                  ? 0
+                  : isValueDashBoard.stt_complete}
+              </span>
+            </div>
           </div>
-        </div>
-        <div style={{ display: "flex", paddingTop: 10 }}>
-          <div style={{ width: "50%", display: "flex", alignItems: "center" }}>
-            <Tag color="#577354" style={{ width: 40, height: 20 }}></Tag>
-            <span
-              className="management-dashboard-span-status"
-              data-status-color-verified={true}
-              data-mobile={true}
+          <div style={{ display: "flex", paddingTop: 10 }}>
+            <div
+              style={{ width: "50%", display: "flex", alignItems: "center" }}
             >
-              {fileLanguage[chooseLanguage].verified}
-            </span>
-          </div>
-          <div
-            style={{
-              width: "50%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <span
-              className="management-dashboard-span-total"
-              data-total-color-verified={true}
-              data-mobile={true}
+              <Tag color="#577354" style={{ width: 40, height: 20 }}></Tag>
+              <span
+                className="management-dashboard-span-status"
+                data-status-color-verified={true}
+                data-mobile={true}
+              >
+                {fileLanguage[chooseLanguage].verified}
+              </span>
+            </div>
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
             >
-              {isValueDashBoard.stt_verified === "[]"
-                ? 0
-                : isValueDashBoard.stt_verified}
-            </span>
+              <span
+                className="management-dashboard-span-total"
+                data-total-color-verified={true}
+                data-mobile={true}
+              >
+                {isValueDashBoard.stt_verified === "[]"
+                  ? 0
+                  : isValueDashBoard.stt_verified}
+              </span>
+            </div>
           </div>
         </div>
       </Col>
