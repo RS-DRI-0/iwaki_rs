c0437c9ecbfe3b3c9d6725cc7c119e01c4c29e5c
hoanglong <ro1462000@gmail.com>
2024-11-21 10:46:38 +0700

    LongLH

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 // export const localhost = "http://42.113.15.62:5001";
 // export const localhost = "http://192.168.30.26:5010"; //app
 
-
 // export const localhost = "http://192.168.30.26:5010"; //op
 
 // export const localhost = "http://192.168.30.26:5012"; //app
@@ -14,13 +13,13 @@
 // export const localhost = "https://iwaki.rainscales.com/api";     //5000 // Build xong nhớ copy lại src
 // export const localhost = "https://iwaki-test1.dri.io.vn/api";
 // export const localhost = "https://truongtx.dri.io.vn/api";
-// export const localhost = "https://longlh.dri.io.vn/api";
+export const localhost = "https://longlh.dri.io.vn/api";
 
 // export const localhost = "https://test-op-iwaki.dri.io.vn/api";
 // export const localhost = "https://test-iwaki.dri.io.vn/api";
 //
-// export const localhost = "https://dev-op-iwaki.rainscales.com/api";
-export const localhost = "https://dev-iwaki.rainscales.com/api";
+// export const localhost = "https://dev-op-iwaki.rainscales.com/api"; //5009
+// export const localhost = "https://dev-iwaki.rainscales.com/api"; //5010
 // export const localhost = "https://op-iwaki.rainscales.com/api";
 // export const localhost = "https://uat-op-iwaki.rainscales.com/api";
 // export const localhost = "https://uat-iwaki.rainscales.com/api";
@@ -32,4 +31,3 @@ export const localhost = "https://dev-iwaki.rainscales.com/api";
 // export const localhost = 'https://scan-test.dominatech.xyz/api' //5009
 // export const localhost = 'https://tailnt.dominatech.xyz/api'
 // export const localhost = "http://192.168.20.190:8888";
-// export const localhost = "https://longlh.dominatech.xyz/api";
